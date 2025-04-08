b8d5344363ea94fd4bd05ca17609a28572ddea89
V1aD1slavee <vladboulderchannel@gmail.com>
2025-04-08 21:39:54 +0200

    commit

--- it_bot.py
+++ it_bot.py
@@ -10,16 +10,18 @@ dp = Dispatcher()
 logging.basicConfig(level=logging.INFO)
 
 start_buttons = [
-    types.KeyboardButton(text="О нас"),
-    types.KeyboardButton(text="Курсы"),
-    types.KeyboardButton(text="Адрес"),
-    types.KeyboardButton(text="Контакты")
+    [types.KeyboardButton(text="О нас"), types.KeyboardButton(text="Курсы")],
+    [types.KeyboardButton(text="Адрес"), types.KeyboardButton(text="Контакты")],
 ]
+
+start_keyboard = types.ReplyKeyboardMarkup(keyboard=start_buttons, resize_keyboard=True)
+# ReplyKeyboardMarkup – клавиатура, которая показывается вместо основной
+# и не привязана ни к какому сообщению. Представляет собой шаблоны сообщений
+# (варианты ответа), которые выбираются путем нажатия на готовую кнопку.
 
 @dp.message(Command("start"))
 async def start(message:Message):
-    await message.answer(f"Здравствуйте {message.from_user.full_name}")
-
+    await message.answer(f"Здравствуйте {message.from_user.full_name}", reply_markup=start_keyboard)
 
 async def main():
     await dp.start_polling(bot)
