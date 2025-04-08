59af7c611cb66975f4f6c6fd412f54d83f011e37
V1aD1slavee <vladboulderchannel@gmail.com>
2025-04-08 21:07:02 +0200

    старт работы над ботом

--- it_bot.py
+++ it_bot.py
@@ -1,4 +1,29 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message
 from aiogram.filters import Command
-import logging
+import logging, asyncio
+from config import token
+
+bot = Bot(token=token)
+dp = Dispatcher()
+
+logging.basicConfig(level=logging.INFO)
+
+start_buttons = [
+    types.KeyboardButton(text="О нас"),
+    types.KeyboardButton(text="Курсы"),
+    types.KeyboardButton(text="Адрес"),
+    types.KeyboardButton(text="Контакты")
+]
+
+@dp.message(Command("start"))
+async def start(message:Message):
+    await message.answer(f"Здравствуйте {message.from_user.full_name}")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
