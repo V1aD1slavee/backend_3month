976e035638a08df156049a57f16a06e0e409e5fa
V1aD1slavee <vladboulderchannel@gmail.com>
2025-05-13 18:23:56 +0300

    стягивание несколько страниц

--- parsing.py
+++ parsing.py
@@ -1,15 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://24.kg'
-response = requests.get(url=url)
-# print(response)
+for page in range(1, 11):
+    url = f'https://24.kg/page_{page}'
+    response = requests.get(url=url)
+    # print(response)
 
-soup = BeautifulSoup(response.text, 'lxml')
-all_news = soup.find_all('div', class_='title')
-# print(all_news)
+    soup = BeautifulSoup(response.text, 'lxml')
+    all_news = soup.find_all('div', class_='title')
+    # print(all_news)
 
-count_news = 0
-for news in all_news:
-    count_news += 1
-    print(count_news,news.text)
+    count_news = 0
+    for news in all_news:
+        count_news += 1
+        print(count_news,news.text)
