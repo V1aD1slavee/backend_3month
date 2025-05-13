4760e7dfa6099e0362c9ebf78c83b84bb1e45397
V1aD1slavee <vladboulderchannel@gmail.com>
2025-05-13 18:43:11 +0300

    урок окончен, дз создать телеграмм бота который парсит по комманде новости и с коммандой стоп

--- parsing.py
+++ parsing.py
@@ -1,16 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
 
-for page in range(1, 11):
-    url = f'https://24.kg/page_{page}'
-    response = requests.get(url=url)
-    # print(response)
 
-    soup = BeautifulSoup(response.text, 'lxml')
-    all_news = soup.find_all('div', class_='title')
-    # print(all_news)
+count_news = 0
+with open("news.txt", 'w', encoding='UTF-8') as file:
+    for page in range(1, 21):
+        url = f'https://24.kg/page_{page}'
+        response = requests.get(url=url)
+        # print(response)
 
-    count_news = 0
-    for news in all_news:
-        count_news += 1
-        print(count_news,news.text)
+        soup = BeautifulSoup(response.text, 'lxml')
+        all_news = soup.find_all('div', class_='title')
+        # print(all_news)
+
+        for news in all_news:
+            count_news += 1
+            file.write(f'{count_news} {news.text}\n')
+            print(count_news,news.text)
