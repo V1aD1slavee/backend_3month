1ab850aa87e2962f6cdadcdf10c560d26d1f5d66
V1aD1slavee <vladboulderchannel@gmail.com>
2025-05-13 18:08:34 +0300

    гет запрос

--- parsing.py
+++ parsing.py
@@ -1,5 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://24.kg/"
+url = 'https://24.kg'
 response = requests.get(url=url)
+# print(response)
+
+soup = BeautifulSoup(response.text, 'lxml')
+all_news = soup.find_all('div', class_='title')
+# print(all_news)
+
+count_news = 0
+for news in all_news:
+    count_news += 1
+    print(count_news,news.text)
