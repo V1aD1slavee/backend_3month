4f8616c1afb9ed8219b90f51278dba4b322623d6
V1aD1slavee <vladboulderchannel@gmail.com>
2025-05-15 13:16:07 +0300

    commit

--- parsing_bot.py
+++ parsing_bot.py
@@ -1,23 +1,18 @@
 from aiogram import Bot, Dispatcher, types, F
-from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import State, StatesGroup
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
 from bs4 import BeautifulSoup
-import requests, logging, asyncio
+import requests, logging, asyncio, threading, time
 from config import token
 
 bot = Bot(token=token)
-storage = MemoryStorage()
-dp = Dispatcher(storage=storage)
+dp = Dispatcher()
 
 logging.basicConfig(level=logging.INFO)
 
-class Form(StatesGroup):
-    number_of_pages = State()
+user_threading = {}  # хранит активные потоки
+stop_flags = {}  # стоп флаги
 
-@dp.message(CommandStart())
-async def start(message:Message, state:FSMContext):
-    await message.answer("Привет, я бот по парсингу сайтов")
+def get_news():
+    url = ''
