5feafc27221718ec9e18eba9b9e9d4c3a725391a
V1aD1slavee <vladboulderchannel@gmail.com>
2025-05-29 15:44:10 +0600

    метод для парсинга

--- aioschedule.py
+++ aioschedule.py
@@ -1,9 +1,16 @@
 import schedule
 import time
+import requests
 
 def test():
     print("Hello world")
     print(time.ctime())
+
+def get_btc_price():
+    print("======BTC======")
+    url = "https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT"
+    response = requests.get(url=url).json()
+    print(response)
 
 # schedule.every(2).seconds.do(test)
 # schedule.every(1).minutes.do(test)
