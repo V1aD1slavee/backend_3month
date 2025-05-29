75fb6035432cc9ab6b5f8cc60b77bc62f1e8b7b9
V1aD1slavee <vladboulderchannel@gmail.com>
2025-05-29 16:07:27 +0600

    тг бот для парсинга цены бтс

---
+++ bot_btc.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message, BotCommand
+
+from config import token
+import requests, time, asyncio, aioschedule, logging
+
+bot = Bot(token=token)
+dp = Dispatcher()
+logging.basicConfig(level=logging.INFO)
+
+def get_btc_price():
+    url = "https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT"
+    response = requests.get(url=url).json()
+    price = response.get('price')
+    if price:
+        return f"Стоимость биткоина на {time.ctime()}, {price} долларов"
+    else:
+        return "Не удалось получить цену биткоина"
+
+
