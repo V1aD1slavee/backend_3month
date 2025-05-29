2393ba3aa1cac40005cd7bfe8d2ca23dbd9adc4e
V1aD1slavee <vladboulderchannel@gmail.com>
2025-05-29 12:58:53 +0600

    commit

--- aioschedule.py
+++ aioschedule.py
@@ -5,7 +5,9 @@ def test():
     print("Hello world")
     print(time.ctime())
 
-schedule.every(2).seconds.do(test)
+# schedule.every(2).seconds.do(test)
+# schedule.every(1).minutes.do(test)
+# schedule.every().day.at('12:58').do(test)
 
 while True:
     schedule.run_pending()
