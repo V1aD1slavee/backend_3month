0a9f38cc0b9720a30c8e80f778e939c124b98670
V1aD1slavee <vladboulderchannel@gmail.com>
2025-05-29 15:47:02 +0600

    использование парсинга с библиотекой aioschedule

--- aioschedule.py
+++ aioschedule.py
@@ -12,12 +12,14 @@ def get_btc_price():
     response = requests.get(url=url).json()
     print(response)
 
+# get_btc_price()
 # schedule.every(2).seconds.do(test)
 # schedule.every(1).minutes.do(test)
 # schedule.every().day.at('12:58').do(test)
 # schedule.every().thursday.at('14:56').do(test)
 # schedule.every().day.at('19:30', 'Europe/Moscow').do(test)
 # schedule.every().hour.at(':25').do(test)
+schedule.every(2).seconds.do(get_btc_price)
 
 while True:
     schedule.run_pending()
