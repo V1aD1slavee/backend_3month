9dbd024291b9c8fc1b988f3ad4d22d2b506a36c1
V1aD1slavee <vladboulderchannel@gmail.com>
2025-05-29 15:10:23 +0600

    1

--- aioschedule.py
+++ aioschedule.py
@@ -8,6 +8,8 @@ def test():
 # schedule.every(2).seconds.do(test)
 # schedule.every(1).minutes.do(test)
 # schedule.every().day.at('12:58').do(test)
+# schedule.every().thursday.at('14:56').do(test)
+# schedule.every().day.at('19:30', 'Europe/Moscow').do(test)
 
 while True:
     schedule.run_pending()
