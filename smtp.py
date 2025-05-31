1ba1688b375f1ecaf51ebbbfc9ae5291c8484174
V1aD1slavee <vladboulderchannel@gmail.com>
2025-05-31 11:17:22 +0600

    функция отправить эмаил

---
+++ smtp.py
@@ -0,0 +1,9 @@
+import smtplib
+from config import smtp_sender, smtp_sender_password
+
+def send_email(to_email, subject, message):
+    sender = smtp_sender
+    password = smtp_sender_password
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    
